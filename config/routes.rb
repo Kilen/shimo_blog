190f483eb032aa41e65eafbabb10dca593f451c7
kilen <zhilin.eno@gmail.com>
2014-11-08 13:41:11 +0800

    styles thing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :articles
   post 'upload/image' => 'upload#image', as: :upload_image
 
+  get '/about' => 'pages#about', as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
